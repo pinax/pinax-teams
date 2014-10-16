793fd17bd781fcc9275bab748915a92bb589d18a
Patrick Altman <paltman@gmail.com>
2014-10-15 21:10:31 -0500

    Adding signals module was missing from previous commit

---
+++ teams/signals.py
@@ -0,0 +1,11 @@
+import django.dispatch
+
+
+added_member = django.dispatch.Signal(providing_args=["membership"])
+invited_user = django.dispatch.Signal(providing_args=["membership"])
+promoted_member = django.dispatch.Signal(providing_args=["membership"])
+demoted_member = django.dispatch.Signal(providing_args=["membership"])
+accepted_membership = django.dispatch.Signal(providing_args=["membership"])
+rejected_membership = django.dispatch.Signal(providing_args=["membership"])
+resent_invite = django.dispatch.Signal(providing_args=["membership"])
+removed_membership = django.dispatch.Signal(providing_args=["membership"])
