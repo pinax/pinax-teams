12defe0e3d3a843b9ec3ed7d902aea66681e5e48
Patrick Altman <paltman@gmail.com>
2014-10-28 14:49:26 -0500

    Fix up admin to be a bit more useful

--- teams/admin.py
+++ teams/admin.py
@@ -5,14 +5,30 @@ import reversion
 from .models import Team, Membership
 
 
+def members_count(obj):
+    return obj.memberships.count()
+members_count.short_description = "Members Count"
+
+
 admin.site.register(
     Team,
-    list_display=["name", "member_access", "manager_access", "creator"],
+    list_display=["name", "member_access", "manager_access", members_count, "creator"],
+    fields=[
+        "name",
+        "slug",
+        "avatar",
+        "description",
+        "member_access",
+        "manager_access",
+        "creator"
+    ],
     prepopulated_fields={"slug": ("name",)},
+    raw_id_fields=["creator"]
 )
 
 
 class MembershipAdmin(reversion.VersionAdmin):
+    raw_id_fields = ["user"]
     list_display = ["team", "user", "state", "role"]
     list_filter = ["team"]
     search_fields = ["user__username"]
